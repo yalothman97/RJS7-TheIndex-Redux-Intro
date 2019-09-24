shorookMS/TheIndex-Chapter7-Redux-Intro

removed newAuthId from mapStateToProps in App.js

src/App.js
@@ -35,8 +35,7 @@ class App extends Component {
 
 const mapStateToProps = state => {
   return {
-    auths: state.authors,
-    newAuthId: state.newAuthorId
+    auths: state.authors
   };
 };
 

yalothman97/RJS7-TheIndex-Redux-Intro

src/App.js
@@ -34,8 +34,7 @@ class App extends Component {
 
 const mapStateToProps = state => {
   return {
-    authors: state.authors,
-    newduthorId: state.newAuthorId
+    authors: state.authors
   };
 };
 